01fda1d2559685b16dfb0e0528be2625a5ad2cf7
RocCorral <ROC.webdev@gmail.com>
2018-09-13 21:30:25 -0600

    comments ade

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,3 +1,16 @@
+"""
+@author Rocorral
+ID: 80416750
+Instructor: David Aguirre
+Assignment:Lab 1-B - Recursion
+Last Modification: 09/13/2018
+Program Purpose: The purpose of this program is to practice recursive algorithm problem
+solving andbuilding. by Using methods built in to the provided NLTK and PRAW software we 
+can access Reddit submission and break down their comment and reply trees into traversable 
+data. We must implement a recursive algorithm to process all comments and designate
+them to appropriate lists based on the results of NLTK's Sentiment analysis.
+"""
+
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import praw
@@ -29,38 +42,39 @@ def get_submission_comments(url):
     submission.comments.replace_more()
 
     return submission.comments
-
+#Begin My Code
 def process_comments(comments,c):
-
-	
+	#base case returns after reaching maximum reply depth + 1.
 	if c==len(comments) or comments is None:
 		return
-	
+	#test the comment for Sentiment scores
 	pos = get_text_positive_proba(comments[c].body)
 	neut = get_text_neutral_proba(comments[c].body)
 	neg = get_text_negative_proba(comments[c].body)
-
-	
+	#passes sentiment scores and comment to sorting def
 	list_sorting(comments[c].body,pos,neut,neg)
-
+	#recursive call passing the current comments first reply making it current comment in the next call getting one level deeper into the replies.  
 	process_comments(comments[c].replies,0)
+	#recursive call remaining in current comment depth but next comment down list
 	process_comments(comments,c+1)
-
+#sorting Def taking in scores and comment to be placed in appropriate list based on score
 def list_sorting(cmnt,pos_score,neut_score,neg_score):
 	if pos_score > neut_score >neg_score:
-		pos_c_list.append(cmnt)
-		print("this comment was added to Pos  list with a Pos Score of",pos_score," a neg score of ",neg_score," and a neut_score of ",neut_score," -------->")
+		positive_comments_list.append(cmnt)
+		#print("this comment was added to Pos  list with a Pos Score of",pos_score," a neg score of ",neg_score," and a neut_score of ",neut_score," -------->")
 	if neut_score > pos_score >neg_score:
-		neu_c_list.append(cmnt)
-		print("this comment was added to Neut list with a Pos Score of",pos_score," a neg score of ",neg_score," and a neut_score of ",neut_score," -------->")
+		neutral_comments_list.append(cmnt)
+		#print("this comment was added to Neut list with a Pos Score of",pos_score," a neg score of ",neg_score," and a neut_score of ",neut_score," -------->")
 	if neg_score > pos_score > neut_score:
-		neg_c_list.append(cmnt)
-		print("this comment was added to Neg  list with a Pos Score of",pos_score," a neg score of ",neg_score," and a neut_score of ",neut_score," -------->")
+		negative_comments_list.append(cmnt)
+		#print("this comment was added to Neg  list with a Pos Score of",pos_score," a neg score of ",neg_score," and a neut_score of ",neut_score," -------->")
 
-pos_c_list = []
-neu_c_list = []
-neg_c_list =[]
 
+positive_comments_list = []
+neutral_comments_list = []
+negative_comments_list = []
+#the following thread was chosen for the usual emotional charge in political conversation
 comments = get_submission_comments('https://www.reddit.com/r/Conservative/comments/9clfbn/the_horror/')
 process_comments(comments,0)
+
 
